9d51209454557c84d52bfd9cad648477e97b76c3
hclee <heechul4296@gmail.com>
2022-02-27 12:34:18 +0900

    http 요청 파라미터 - @RequestParam

--- src/main/java/study/springmvc/http/basic/request/RequestParamController.java
+++ src/main/java/study/springmvc/http/basic/request/RequestParamController.java
@@ -3,17 +3,22 @@ package study.springmvc.http.basic.request;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
 public class RequestParamController {
 
     @RequestMapping(value = "/request-param-v1")
-    public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void requestParamV1(
+            HttpServletRequest request,
+            HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         int age = Integer.parseInt(request.getParameter("age"));
 
@@ -21,4 +26,68 @@ public class RequestParamController {
 
         response.getWriter().write("ok");
     }
+
+    @RequestMapping(value = "/request-param-v2")
+    @ResponseBody
+    public String requestParamV2(
+            @RequestParam("username") String username,
+            @RequestParam("age") int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @RequestMapping(value = "/request-param-v3")
+    @ResponseBody
+    public String requestParamV3(
+            @RequestParam String username,
+            @RequestParam int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @RequestMapping(value = "/request-param-v4")
+    @ResponseBody
+    public String requestParamV4(
+            String username,
+            int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @RequestMapping(value = "/request-param-required")
+    @ResponseBody
+    public String requestParamRequired(
+            @RequestParam(required = true) String username,
+            @RequestParam(required = false) int age) {
+        // int a = null -> 기본형(primitive) 안됨
+        // Integer a = null -> 객체형이기 때문에 됨
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @RequestMapping(value = "/request-param-default")
+    @ResponseBody
+    public String requestParamDefault(
+            @RequestParam(required = true, defaultValue = "guest") String username,
+            @RequestParam(required = false, defaultValue = "-1") int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @RequestMapping(value = "/request-param-map")
+    @ResponseBody
+    public String requestParamMap(
+            @RequestParam Map<String, Object> paramMap) {
+        log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
+    }
+
+
+
+
+
+
+
+
+
 }
